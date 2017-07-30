e2ab2c0651f3c8e2b8a5fdc5ade1f0dd925cd494
Viktor Burka <viktor.cpp@gmail.com>
2017-07-29 20:31:27 -0400

    Added WtPrint and WtColor documentation

--- wt.cpp
+++ wt.cpp
@@ -5,11 +5,41 @@
 
 using namespace Wt;
 
+/*!
+    \class WtPrint
+    \inmodule Wt
+
+    \brief The WtPrint class prints application output.
+
+    \ingroup tools
+
+    WtPrint can print application output. It prints to the console by default
+    and has a veriety of overloaded methods that understand different types.
+
+    Using WtPrint is easy just call it by using stream \a << operator like this:
+
+    \code Rect rect(0,0,100,100);
+    \code ...
+    \code WtPrint() << "This is my widget rectangle:" << rect;
+
+    In this eaxmples it takes char* first and then structure of type Rect and
+    conveniently prints it eleminating the need to put long statements to print
+    each property of Rect like \a x, \a y, \a width, \a height.
+
+    Once WtPrint is finished, it will conveniently put a new line.
+*/
+
+/*!
+    WtPrint destructor. Conveniently puts newline to start next print statement.
+*/
 WtPrint::~WtPrint()
 {
     fprintf(stderr, "\n");
 }
 
+/*!
+    Stream overloaded operator for printing Rect type.
+*/
 WtPrint& WtPrint::operator<<(const Rect & rect)
 {
     fprintf(stderr, "Rect(%d, %d, %d, %d) ", rect.x(), rect.y(),
@@ -17,17 +47,26 @@ WtPrint& WtPrint::operator<<(const Rect & rect)
     return *this;
 }
 
+/*!
+    Stream overloaded operator for printing int type.
+*/
 WtPrint& WtPrint::operator<<(int number)
 {
     fprintf(stderr, "%d ", number);
     return *this;
 }
 
+/*!
+    Stream overloaded operator for printing char* type.
+*/
 WtPrint& WtPrint::operator<<(const char* str)
 {
     fprintf(stderr, "%s ", str);
 }
 
+/*!
+    Stream overloaded operator for printing std::string type.
+*/
 WtPrint& WtPrint::operator<<(const std::string & str)
 {
     fprintf(stderr, "%s ", str.c_str());
--- wtcolor.cpp
+++ wtcolor.cpp
@@ -5,6 +5,29 @@
 
 using namespace Wt;
 
+/*!
+    \class WtColor
+    \inmodule Wt
+
+    \brief The WtColor class implements operations with colors.
+
+    \ingroup widgets
+
+    WtColor works with colors and converts them from abstract platform
+    independent representation to a platform dependent one. It can operate
+    with conventional colors like red, green, blue, etc representing them
+    in more complex display system aware colors based on the given palette.
+
+    Using WtColor is easy. Just create it passing a conventional color name
+    to its constructor like:
+
+    \code Color red("red");
+
+*/
+
+/*!
+    WtColor constructor. Takes conventional color name as parameter.
+*/
 WtColor::WtColor(const std::string & name)
     : r(0x00), g(0x00), b(0x00), a(0x00)
 {
@@ -21,11 +44,17 @@ WtColor::WtColor(const std::string & name)
     m_bgra[0] = b; m_bgra[1] = g; m_bgra[2] = r;
 }
 
+/*!
+    Returns RGBA representation of color.
+*/
 const char* WtColor::rgba() const
 {
     return m_rgba;
 }
 
+/*!
+    Returns BGRA representation of color.
+*/
 const char* WtColor::bgra() const
 {
     return m_bgra;
